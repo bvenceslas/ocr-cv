a9c42849a61fdb8a51111103f975ffeb04cadcdb
bvenceslas <bvenceslas@gmail.com>
2021-08-20 20:14:23 +0200

    fix the warnings

--- src/Components/Works/index.jsx
+++ src/Components/Works/index.jsx
@@ -13,28 +13,28 @@ export function Works(){
                 <div className="my-project">
                     <a href="https://chapafrik.netlify.com" target="_blank" rel="noopener noreferrer">  
                         <div className="ptitle">Hng 8</div>
-                        <img src={project} alt="project image"/>
+                        <img src={project} alt="project_image"/>
                     </a>
                 </div>
                 
                 <div className="my-project">
                     <a href="https://chapafrik.netlify.com" target="_blank" rel="noopener noreferrer">  
                         <div className="ptitle">Mascar</div>
-                        <img src={project} alt="project image" />
+                        <img src={project} alt="project_image" />
                     </a>                    
                 </div>
 
                 <div className="my-project">
                     <a href="https://chapafrik.netlify.com" target="_blank" rel="noopener noreferrer">  
                         <div className="ptitle">Oasis</div>
-                        <img src={project} alt="project image"/>
+                        <img src={project} alt="project_image"/>
                     </a>
                 </div>
                 
                 <div className="my-project">
                     <a href="https://chapafrik.netlify.com" target="_blank" rel="noopener noreferrer">  
                         <div className="ptitle">Scola</div>
-                        <img src={project} alt="project image" />
+                        <img src={project} alt="project_image" />
                     </a>                    
                 </div>
                 
